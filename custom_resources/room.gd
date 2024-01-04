64f6e04618d967e75ebf9de7d7ef442ed724ecbc
Adam Gulacsi <adam.gulacsi@gmail.com>
2024-01-04 07:15:28 +0100

    quick fix: deleted temporary code

--- custom_resources/room.gd
+++ custom_resources/room.gd
@@ -8,14 +8,3 @@ var row: int
 var column: int
 var position: Vector2
 var next_rooms: Array[Room]
-
-
-func _to_string() -> String:
-	return "Room%s(%6s)" % [row*7 + column, _get_connections()]
-
-
-func _get_connections() -> String:
-	var nums: PackedStringArray = []
-	for room in next_rooms:
-		nums.append(str(room.row*7 + room.column))
-	return "-".join(nums)
