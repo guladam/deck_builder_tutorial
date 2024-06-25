25ce072bfce9a9dab4c29fbb8f4f202222f45115
Adam Gulacsi <adam.gulacsi@gmail.com>
2024-06-25 09:40:12 +0200

    #12 fixed

--- scenes/ui/battle_over_panel.gd
+++ scenes/ui/battle_over_panel.gd
@@ -1,22 +1,24 @@
 class_name BattleOverPanel
 extends Panel
 
+const MAIN_MENU = "res://scenes/ui/main_menu.tscn"
+
 enum Type {WIN, LOSE}
 
 @onready var label: Label = %Label
 @onready var continue_button: Button = %ContinueButton
-@onready var restart_button: Button = %RestartButton
+@onready var main_menu_button: Button = %MainMenuButton
 
 
 func _ready() -> void:
 	continue_button.pressed.connect(func(): Events.battle_won.emit())
-	restart_button.pressed.connect(get_tree().reload_current_scene)
+	main_menu_button.pressed.connect(get_tree().change_scene_to_file.bind(MAIN_MENU))
 	Events.battle_over_screen_requested.connect(show_screen)
 
 
 func show_screen(text: String, type: Type) -> void:
 	label.text = text
 	continue_button.visible = type == Type.WIN
-	restart_button.visible = type == Type.LOSE
+	main_menu_button.visible = type == Type.LOSE
 	show()
 	get_tree().paused = true
