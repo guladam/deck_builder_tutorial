c0156168196dd65de148f2b4a6266c04ceed924a
Adam Gulacsi <adam.gulacsi@gmail.com>
2024-06-25 08:28:17 +0200

    #3 fixed

--- scenes/campfire/campfire.gd
+++ scenes/campfire/campfire.gd
@@ -4,9 +4,11 @@ extends Control
 @export var char_stats: CharacterStats
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var rest_button: Button = %RestButton
 
 
 func _on_rest_button_pressed() -> void:
+	rest_button.disabled = true
 	char_stats.heal(ceili(char_stats.max_health * 0.3))
 	animation_player.play("fade_out")
 
