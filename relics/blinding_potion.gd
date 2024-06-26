414cd5fc4151a9b41350dcf981d9baed208decd4
Adam Gulacsi <adam.gulacsi@gmail.com>
2024-06-26 16:47:44 +0200

    extra content: blinding potion v2

--- relics/blinding_potion.gd
+++ relics/blinding_potion.gd
@@ -1,10 +1,18 @@
 extends Relic
 
+var already_initialized := false
+
 
 func initialize_relic(owner: RelicUI) -> void:
+	# makes sure we don't have extra mana when we
+	# keep saving and loading the game
+	if already_initialized:
+		return
+
 	var run := owner.get_tree().get_first_node_in_group("run") as Run
 	run.character.max_mana += 1
 	run.character.mana = run.character.max_mana
+	already_initialized = true
 
 
 func activate_relic(owner: RelicUI) -> void:
@@ -12,6 +20,5 @@ func activate_relic(owner: RelicUI) -> void:
 
 
 func deactivate_relic(owner: RelicUI) -> void:
-	print("woohoo")
 	var run := owner.get_tree().get_first_node_in_group("run") as Run
 	run.character.max_mana -= 1
--- relics/endless_healing_potion.gd
+++
@@ -1,7 +0,0 @@
-extends Relic
-
-
-func activate_relic(owner: RelicUI) -> void:
-	var player := owner.get_tree().get_first_node_in_group("player") as Player
-	player.stats.heal(6)
-	owner.flash()
