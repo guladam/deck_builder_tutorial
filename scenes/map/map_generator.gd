bbfe7b234d084c2370446780d8ea8e8dfa64e81a
Adam Gulacsi <adam.gulacsi@gmail.com>
2024-08-22 08:29:16 +0200

    updated the project to 4.3

--- scenes/map/map_generator.gd
+++ scenes/map/map_generator.gd
@@ -87,7 +87,7 @@ func _get_random_starting_points() -> Array[int]:
 
 
 func _setup_connection(i: int, j: int) -> int:
-	var next_room: Room
+	var next_room: Room = null
 	var current_room := map_data[i][j] as Room
 	
 	while not next_room or _would_cross_existing_path(i, j, next_room):
